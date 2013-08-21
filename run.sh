2ce264028cbe588994dc4297efc9786fe9e0efbd
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2013-08-21 04:38:24 +0000

    Fished epyphany scrypt

--- run.sh
+++ run.sh
@@ -7,5 +7,4 @@ ELIBS=${ESDK}/tools/host/lib:${LD_LIBRARY_PATH}
 EHDF=${EPIPHANY_HDF}
 
 sudo -E LD_LIBRARY_PATH=${ELIBS} EPIPHANY_HDF=${EHDF} ./cgminer -o http://ltc.kattare.com:9332 --scrypt -u lordrafa.1 -p 123456 -T
-
 exit 0
